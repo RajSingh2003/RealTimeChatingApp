1dca7ebaa4101a0a51a171823d2c6670898eb313
RajSingh2003 <rajabhaysingh742@gmail.com>
2025-07-27 19:24:10 +0530

    first commit

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,22 +1,21 @@
 import uploadOnCloudinary from "../config/cloudinary.js"
 import User from "../models/user.model.js"
 
-export const getCurrentUser = async(req, res) => {
-    try {
-        let userId = req.userId; // Extracted from the isAuth middleware
-        let user=await User.findById(req.userId).select("-password") // Exclude password from the response
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }   
-        return res.status(200).json(user);
-       
-    } catch (error) {
-        return res.status(500).json({ message: `Error: ${error}` });
+export const getCurrentUser=async (req,res)=>{
+try {
+    let user=await User.findById(req.userId).select("-password")
+    if(!user){
+        return res.status(400).json({message:"user not found"})
     }
+
+    return res.status(200).json(user)
+} catch (error) {
+    return res.status(500).json({message:`current user error ${error}`})
+}
 }
 
-export const editProfile = async(req, res) => {
-     try {
+export const editProfile=async (req,res)=>{
+    try {
         let {name}=req.body
         let image;
         if(req.file){
@@ -48,7 +47,7 @@ export const getOtherUsers=async (req,res)=>{
     }
 }
 
- export const search =async (req,res)=>{
+export const search =async (req,res)=>{
     try {
         let {query}=req.query
         if(!query){
